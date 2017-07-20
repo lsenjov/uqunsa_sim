a91ace7ad30e81b539be886d5ee42f55db5b4670
Logan Senjov <logansenjov@gmail.com>
2017-07-20 14:26:54 +1000

    Adds regions database namespace

---
+++ src/clj/uqunsa_sim/db/regions.clj
@@ -0,0 +1,39 @@
+(ns uqunsa-sim.db.regions
+  (:require [uqunsa-sim.db.core :as db]
+            [uqunsa-sim.model.shared :as m-shared]
+            [uqunsa-sim.model.region :as m-region]
+            ))
+
+(db/get-all-regions)
+
+(defn get-all-regions
+  "Returns a collection of all regions"
+  []
+  (->> (db/get-all-regions)
+       (map (comp clojure.edn/read-string :object))
+       ))
+(defn store-region
+  "Stores a region in the database.
+  NOTE: If the region uid already exists, it will overwrite the old region with the new one. YOU HAVE BEEN WARNED."
+  [{::m-shared/keys [uid uname]
+    ::m-region/keys [links] ; Links is unused, but a reminder it still exists
+    {position-x ::m-region/position-x position-y ::m-region/position-y} ::m-region/position
+    :as object
+    }]
+  (db/create-region!
+    {:id uid
+     :name uname
+     :regionx position-x
+     :regiony position-y
+     :object (pr-str object)}))
+(defn get-region
+  "Returns either a region with the same uid/uname, or nil"
+  [search]
+  (first (db/get-region {:search search})))
+
+(comment
+  (get-all-regions)
+  (store-region (m-region/create-region "Test-region" 90 40 {"Antartica" 8}))
+  (get-region "Test-region")
+  (get-region "Unknown")
+  )
